647cf94fbb05edab44e3d8cb8a9104190098d3e2
Franck De Goër <degoerdf@ensimag.fr>
2013-07-06 12:30:10 +0200

    Gallery view

--- web/gallery/views.py
+++ web/gallery/views.py
@@ -1,14 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from django.shortcuts import render
 
 import os.path
 
-def hello_world(request):
-    template_name = "helloWorld.html"
+# View for the homepage
+def home(request):
+    template_name = "home.html"
 
     context = dict()
     context["title"] = "WIS - Accueil"
-    context["body"] = "<div id=\"testBox\"> Ahaha </div>"
+
+    return render(request, template_name, context)
+
+
+# View for a personal gallery home page
+def gallery_home(request, name):
+    template_name = "gallery.html"
+
+    if (int(name) > 2000): 
+        raise Http404
+
+    context = dict()
+    context["title"] = name
 
     return render(request, template_name, context)
