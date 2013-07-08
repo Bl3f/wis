59f4f62028775ac023e026b73810e4828aaf5ce7
Bl3f <christophe.blefari@gmail.com>
2013-07-08 11:26:16 +0200

    Structure changes + first gallery view without database

---
+++ apps/gallery/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+
+from django.shortcuts import render
+from django.views.generic import ListView
+import os
+from web.settings import MEDIA_ROOT
+
+
+class GalleryListView(ListView):
+    context_object_name = "pics_gallery"
+    template_name = "gallery.html"
+
+    def get_queryset(self):
+        self.my_list = []
+        files = os.listdir(MEDIA_ROOT + 'pic/base/')
+        self.my_list = files
+
+    def get_context_data(self, **kwargs):
+        context = super(GalleryListView, self).get_context_data(**kwargs)
+        context['my_list'] = self.my_list
+        return context
+
+
+def hello_world(request):
+    template_name = "helloWorld.html"
+
+    context = dict()
+    context["title"] = "Hello World !"
+    context["body"] = "<div id=\"testBox\"> Ahaha </div>"
+
+    return render(request, template_name, context)
--- web/urls.py
+++ web/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import patterns, include, url
-
-import gallery.views
+from django.conf import settings
+from apps.gallery.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-
-    url(r'^$', 'gallery.views.home'),
-    url(r'^gallery/', include('gallery.urls'))
+    url(r'^plop/$', hello_world),
+    url(r'^gallery/$', GalleryListView.as_view())
 
     # Examples:
     # url(r'^$', 'web.views.home', name='home'),
@@ -21,3 +20,14 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^data/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+            }),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+            }),
+    )
