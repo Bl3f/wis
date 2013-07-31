3cb409e77c04af9375b7fac39e9d3d3707336234
Bl3f <christophe.blefari@gmail.com>
2013-07-31 21:54:30 +0200

    New structure with 3 divs

--- web/apps/gallery/static/js/gallery.js
+++ web/apps/gallery/static/js/gallery.js
@@ -2,12 +2,26 @@
 
     "use strict";
 
+    $.fn.get_best = function(){
+        var best = null
+        $('#gallery .column').each(function() {
+            if (best != null) {
+                if (best.height() > $(this).height()) {
+                    best = $(this)
+                }
+            } else {
+                best = $(this)
+            }
+        });
+        return best;
+    };
+
     $('#gallery a img').each(function(index) {
         var img = $(this).attr('src', $(this).attr('data-url')).load(function() {
             if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
-                alert('broken image!');
+                console.log('Broken image.')
             } else {
-                $(this).append(img);
+                $.fn.get_best().append($(this).append(img).parent());
             }
         });
     });
