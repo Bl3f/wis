d2fe9e56e97f6ea1854ae739100c51cd7a804cce
Franck De Goër <degoerdf@ensimag.fr>
2013-08-15 12:05:14 +0200

    Two new messages for accessing a protected gallery

--- web/apps/gallery/messages.py
+++ web/apps/gallery/messages.py
@@ -7,8 +7,10 @@ of his action. """
 # Error messages
 ERROR_PERM = "Erreur : vous n'avez pas la permission d'effectuer cette action."
 ERROR_AUTH = "L'authentification a échoué. "
+ERROR_ACCESS_GALLERY = "Mauvais mot de passe."
 
 # Success messages
 SUCCESS_AUTH = "Vous êtes désormais authentifié."
 SUCCESS_LOGOUT = "Vous êtes déconnecté."
 SUCCESS_GALLERY_CREATION = "La nouvelle galerie a été créée."
+SUCCESS_ACCESS_GALLERY = "Vous pouvez désormais accéder à cette galerie."
